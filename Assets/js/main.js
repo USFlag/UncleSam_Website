258f9ce8a0497d35c757f62ff956e252f2d463a2
Kenneth Stoddard <info@usflagandbanner.com>
2013-06-14 09:39:04 -0400

    Current state of things.

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,4 +1,19 @@
 
 //US Flag and Banner Master JS Script
+$(document).ready(function(){
+	
+	
+	$(".catProdAttributeItem select").change(function(){
+		var selectedValue = $(this).find(":selected").val();
+		console.log(selectedValue);
+		if(selectedValue === "7266182"){
+			console.log(selectedValue);
+			$('.flag-content').load('http://usflagandbanner.businesscatalyst.com/shop/flags/feather-dancer/smiley/business/grand-opening-1 .online-shop');
+			//window.location.href = "http://usflagandbanner.businesscatalyst.com/shop/flags/feather-dancer/smiley/business/grand-opening-1";
+		}
+	});
+	
+	
+	
+});
 
-// Tabbed Panels
