66d9ce340d331745bdab1850cb48107a608f1f11
Kenneth Stoddard <info@usflagandbanner.com>
2013-07-26 15:50:36 -0400

    Ready to Launch

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -2,39 +2,7 @@
 //US Flag and Banner Master JS Script
 $(document).ready(function(){
 	
-/*	
-	var pathname = window.location.pathname;
-	var crumbs = $(".crumbs").html();
-	if( crumbs == '<a href="/shop">shop</a> : <a href="/shop/flags">flags</a> : <a href="/shop/flags/feather-dancer">feather dancer</a> : <a href="/shop/flags/feather-dancer/auto">auto</a>'){
-		$(".flag-layout .product-poplets-flag").remove();
-	}else if ( crumbs == '<a href="/shop">shop</a> : <a href="/shop/banners">banners</a> : <a href="/shop/banners/fireworks">fireworks</a>' ) {
-		$(".banner-layout .product-poplets-banners").remove();
-	}*/
-	
-	//var pathname = window.location.pathname;
-	/*$("select.feather-dancer").on("change", function(){
-		var selectedProduct = $(this).find(":selected").val();
-		console.log(selectedProduct);
-		
-		switch (selectedProduct){
-		
-			case "FDFLAG130101BHPKT":
-			$('.products').load('http://usflagandbanner.businesscatalyst.com/all/flags/buy-here-pay-here-13-flag-kit-cone-head .products');
-			
-			break;
-			
-			case "FDFLAG130101BHP":
-			$('.products').load('http://usflagandbanner.businesscatalyst.com/all/flags/buy-here-pay-here-13-flag-kit-cone-head .products');
-			break;
-			
-			default:
-			break;
-
-		}
-		
-	});*/
-	
-	
+	$(".catalogs:odd .layout-one-half").removeClass("layout-one-half").addClass("layout-one-half-right");
 	
 });
 
