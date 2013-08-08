612aa130861d2ac1c99c8075792d0c7f2784b928
Kenneth Stoddard <info@usflagandbanner.com>
2013-08-08 15:44:36 -0400

    Performance Updates.

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -2,7 +2,7 @@
 //US Flag and Banner Master JS Script
 $(document).ready(function(){
 	
-	$(".catalogs:odd .layout-one-half").removeClass("layout-one-half").addClass("layout-one-half-right");
+	//$(".catalogs:odd .layout-one-half").removeClass("layout-one-half").addClass("layout-one-half-right");
 	
 });
 
