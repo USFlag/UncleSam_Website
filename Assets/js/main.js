ddc574bccc6fce75ac951a345eab0a17662edba7
Kenneth Stoddard <info@usflagandbanner.com>
2013-08-27 08:47:08 -0400

    Performance Updates.

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -4,5 +4,14 @@ $(document).ready(function(){
 	
 	//$(".catalogs:odd .layout-one-half").removeClass("layout-one-half").addClass("layout-one-half-right");
 	
+	msg = $("td.cartSummaryItem").text();
+	
+	if(msg === "Shopping cart is empty."){
+		$("td.cartSummaryItem").hide();
+	}
+	
+    
+
+	
 });
 
